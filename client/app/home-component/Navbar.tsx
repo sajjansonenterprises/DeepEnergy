f2b2d7a73635f4c950a2fc91dde6ad8506da565c
sajjansonenterprises <sajjanson.test@gmail.com>
2025-03-17 15:48:15 +0530

    fix submenu click behaviour

--- client/app/home-component/Navbar.tsx
+++ client/app/home-component/Navbar.tsx
@@ -160,7 +160,7 @@ export default function Navbar() {
                               className={`block px-4 py-2 text-gray-700 hover:bg-gray-50 ${
                                 pathname === subItem.path ? "bg-gray-50 text-green-600" : ""
                               }`}
-                              onClick={closeAllMenus}
+                              onClick={closeAllMenus} // Close all menus when a submenu item is clicked
                             >
                               {subItem.name}
                             </Link>
@@ -246,7 +246,7 @@ export default function Navbar() {
                             key={subItem.id}
                             href={subItem.path}
                             className="block px-3 py-2 text-gray-600 hover:text-green-600"
-                            onClick={closeAllMenus}
+                            onClick={closeAllMenus} // Close all menus when a submenu item is clicked
                           >
                             {subItem.name}
                           </Link>
